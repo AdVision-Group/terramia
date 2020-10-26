89890bbb184780da56bc3b1193df4f1f441b2334
Tomáš Boďa <tominoboda@gmail.com>
2020-10-26 13:54:30 +0100

    Updates

---
+++ precache-manifest.489eea3f6fcd7b7a773b35b18f2698c5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e512114c83db5406c2d95de9804daea3",
+    "url": "/terramia/index.html"
+  },
+  {
+    "revision": "9833ad407c791680940b",
+    "url": "/terramia/static/css/main.9fcc561c.chunk.css"
+  },
+  {
+    "revision": "94ab0f0817e3b5898313",
+    "url": "/terramia/static/js/2.de39bd6d.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/terramia/static/js/2.de39bd6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9833ad407c791680940b",
+    "url": "/terramia/static/js/main.447d2801.chunk.js"
+  },
+  {
+    "revision": "d149b4c742d82dfc7e55",
+    "url": "/terramia/static/js/runtime-main.996e5ef2.js"
+  },
+  {
+    "revision": "502dba17e3a876ecc23b130b175284e3",
+    "url": "/terramia/static/media/family.502dba17.png"
+  },
+  {
+    "revision": "fe10d3b8167d2cbdac62104da5a88d73",
+    "url": "/terramia/static/media/home-image.fe10d3b8.jpg"
+  },
+  {
+    "revision": "9eb91cdb080fc03fa44392133958a437",
+    "url": "/terramia/static/media/item-1.9eb91cdb.png"
+  },
+  {
+    "revision": "4fc13732140241403b0eb7faf90ea4e0",
+    "url": "/terramia/static/media/item-2.4fc13732.png"
+  },
+  {
+    "revision": "3a12cb4972bed2ebef84ae698d95b48d",
+    "url": "/terramia/static/media/item-3.3a12cb49.png"
+  },
+  {
+    "revision": "adc6e46efadba70bd38d08a373321e3d",
+    "url": "/terramia/static/media/item-4.adc6e46e.png"
+  },
+  {
+    "revision": "d403643185d06f2adac47270c9640664",
+    "url": "/terramia/static/media/logo.d4036431.png"
+  },
+  {
+    "revision": "e887766eb6b5a9d16e655701ab9adb85",
+    "url": "/terramia/static/media/oil-1.e887766e.png"
+  },
+  {
+    "revision": "59098c9039e12ddbdb84b4f9d16adcc0",
+    "url": "/terramia/static/media/oil-2.59098c90.png"
+  }
+]);
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Router from "./app/Router";
-
-export default function App() {
-  return (
-    <Router />
-  );
-}
