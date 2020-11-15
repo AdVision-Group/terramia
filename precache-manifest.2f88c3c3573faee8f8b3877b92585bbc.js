7fe40ceb57300b86a4d7bc624fd743f744f1fc2c
Tomáš Boďa <tominoboda@gmail.com>
2020-11-15 22:53:36 +0100

    Updates

--- precache-manifest.2f88c3c3573faee8f8b3877b92585bbc.js
+++ precache-manifest.2f88c3c3573faee8f8b3877b92585bbc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "410c0edd2f2313a7ec3d644a56f10f09",
+    "revision": "64ef0321376fae32ba7eadf53fe68caf",
     "url": "/terramia/index.html"
   },
   {
